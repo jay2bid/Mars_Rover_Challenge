afde6390c424cdc714d7c6e6949789ce5c16da4b
jay2bid <jaybidokwu@gmail.com>
2022-09-25 19:39:16 +0100

    Refactored UserInteface to check for valid input

---
+++ MarsRoverService/Explorer.cs
@@ -0,0 +1,38 @@
+﻿using MarsRoverService;
+
+
+Console.WriteLine("Welcome to the Mars explorer!");
+
+UserInterface explorer = new UserInterface();
+
+int[] grid = explorer.GetPlateauMaxCoordinates();
+Plateau mars = new Plateau(grid);
+mars.maxX = grid[0];
+mars.maxY = grid[1];
+
+
+explorer.GetInitialRoverPostion();
+Rover nasa = new Rover();
+nasa.roverPositionX = explorer.RoverX;
+nasa.roverPositionY = explorer.RoverY;
+nasa.direction = explorer.Roverdirection;
+explorer.GetRoverCommands();
+Command first = new Command(explorer.userInputCommand);
+first.ExecuteCommand(nasa);
+
+Rover roscosmos = new Rover();
+explorer.GetInitialRoverPostion();
+roscosmos.roverPositionX = explorer.RoverX;
+roscosmos.roverPositionY = explorer.RoverY;
+roscosmos.direction = explorer.Roverdirection;
+explorer.GetRoverCommands();
+Command second = new Command(explorer.userInputCommand);
+second.ExecuteCommand(roscosmos);
+
+
+Console.WriteLine(nasa.GetPositon());
+Console.WriteLine(roscosmos.GetPositon());
+
+
+
+
