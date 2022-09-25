e0b4032d6649b43e1c5f7a70defce104f48a0a0a
jay2bid <jaybidokwu@gmail.com>
2022-09-25 19:49:30 +0100

    Refactored Userinterface to check for valid user input

--- MarsRoverService/UserInterface.cs
+++ MarsRoverService/UserInterface.cs
@@ -1,51 +1,96 @@
 ﻿using System;
 namespace MarsRoverService
 {
-	public class UserInterface
-	{
+    public class UserInterface
+    {
         public int RoverX;
         public int RoverY;
         public int PlateauX;
         public int PlateauY;
         public string Roverdirection;
         public string userInputCommand;
-
+        string userInputPlateauSize;
+        public int[] grid;
+        bool directionCheck;
+        string userInputPositon;
         public UserInterface()
-		{
-		}
+        {
+        }
 
-		public int[] GetPlateauMaxCoordinates()
+        public int[] GetPlateauMaxCoordinates()
 
-		{
+        {
 
-            Console.WriteLine("Please enter the upper right hand corner coordinates of the Plateau:");
-            string userInputPlateauSize = Console.ReadLine();
-
-            
-            int[] grid;
-
-            PlateauX = Int32.Parse(userInputPlateauSize.Split(" ")[0]);
-            PlateauY = Int32.Parse(userInputPlateauSize.Split(" ")[1]);
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the max coordinates of the Plateau? (Upper right cordinates in the format x y");
+                    userInputPlateauSize = Console.ReadLine();
+                    PlateauX = Int32.Parse(userInputPlateauSize.Split(" ")[0]);
+                    PlateauY = Int32.Parse(userInputPlateauSize.Split(" ")[1]);
+                }
+                catch
+                {
+                    Console.Write("Incorrect format occurred.\n");
+                }
+            }
+            while (PlateauX < 0 || PlateauY < 0 || userInputPlateauSize.Count() != 3);
             return grid = new int[] { PlateauX, PlateauY };
         }
         public void GetInitialRoverPostion()
         {
+            do
+            {
+                try
+                {
+                    bool directionCheck;
+                    Console.WriteLine("Please enter the initial  coordinates of the rover:");
+                    userInputPositon = Console.ReadLine();
+                    RoverX = Int32.Parse(userInputPositon.Split(" ")[0]);
+                    RoverY = Int32.Parse(userInputPositon.Split(" ")[1]);
+                    Roverdirection = userInputPositon.Split(" ")[2];
+                    Roverdirection = Roverdirection.ToUpper();
 
-            Console.WriteLine("Please enter the initial  coordinates of the rover:");
-            string userInputPositon = Console.ReadLine();
-           RoverX = Int32.Parse(userInputPositon.Split(" ")[0]);
-            RoverY = Int32.Parse(userInputPositon.Split(" ")[1]);
-             Roverdirection = userInputPositon.Split(" ")[2];
-            
+                }
+                catch
+                {
+                    Console.Write("Incorrect format occurred.\n");
+                }
+            }
+            while (RoverX < 0 || RoverY < 0 || !(Roverdirection == "N" || Roverdirection == "E" || Roverdirection == "S" || Roverdirection == "W"));
         }
-        public void GetRoverCommands()
+        public string GetRoverCommands()
         {
 
-            Console.WriteLine("Please enter the command instructions for the rover:");
+            do
+            {
+                Console.WriteLine("Please enter the command instructions for the rover:");
+                userInputCommand = Console.ReadLine();
+                userInputCommand = userInputCommand.ToUpper();
+            }
+            while (!ValidateCommand(userInputCommand));
+            return userInputCommand;
 
-            userInputCommand = Console.ReadLine();
+
+
         }
-    }
+        bool ValidateCommand(string userInputCommand)
+        {
+            string commandTemplate = "LRM";
+            userInputCommand = userInputCommand.ToUpper();
 
+            foreach (char c in userInputCommand)
+                if (commandTemplate.Contains(c.ToString()))
+
+                { return true; }
+
+
+
+            return false;
+        }
+
+    }
 }
+
 
