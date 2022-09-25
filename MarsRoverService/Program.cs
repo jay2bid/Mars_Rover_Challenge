33eabf2afd6173aa1e19036b4e5eb0be264cc84f
jay2bid <jaybidokwu@gmail.com>
2022-09-25 02:31:50 +0100

    Added Program to call user interface

--- MarsRoverService/Program.cs
+++ MarsRoverService/Program.cs
@@ -1,16 +1,5 @@
 ﻿using MarsRoverService;
-/*
-int[] grid = new int[] { 5, 5 };
-Plateau mars = new Plateau(grid);
-Rover nasa = new Rover(1, 2, "N");
-Rover russia = new Rover(3, 3, "E");
-Command first = new Command("LMLMLMLMM");
-first.ExecuteCommand(nasa);
-Command second = new Command("MMRMMRMRRM");
-second.ExecuteCommand(russia);
-Console.WriteLine(russia.GetPositon());
-Console.ReadLine();
-*/
+
 
 Console.WriteLine("Welcome to the Mars explorer");
 
@@ -18,27 +7,30 @@ UserInterface explorer = new UserInterface();
 
 int[] grid =  explorer.GetPlateauMaxCoordinates();
 Plateau mars = new Plateau(grid);
-mars.maxX = grid[0];
-mars.maxY = grid[1];
-Rover nasa = new Rover();
+    mars.maxX = grid[0];
+    mars.maxY = grid[1];
+
 explorer.GetInitialRoverPostion();
-nasa.x = explorer.RoverX;
-nasa.y = explorer.RoverY;
-nasa.direction = explorer.Roverdirection;
+Rover nasa = new Rover();
+    nasa.x = explorer.RoverX;
+    nasa.y = explorer.RoverY;
+    nasa.direction = explorer.Roverdirection;
 explorer.GetRoverCommands();
-Command action = new Command(explorer.userInputCommand);
-action.ExecuteCommand(nasa);
+Command first = new Command(explorer.userInputCommand);
+    first.ExecuteCommand(nasa);
+
+Rover roscosmos = new Rover();
+    explorer.GetInitialRoverPostion();
+    roscosmos.x = explorer.RoverX;
+    roscosmos.y = explorer.RoverY;
+    roscosmos.direction = explorer.Roverdirection;
+explorer.GetRoverCommands();
+Command second = new Command(explorer.userInputCommand);
+    second.ExecuteCommand(roscosmos);
+
+
 Console.WriteLine(nasa.GetPositon());
-
-
-
-/*Rover nasa = new Rover(x, y, direction);
-string commandmessage;
-explorer.GetRoverCommands(out commandmessage);
-Command userCommand = new Command( commandmessage);
-userCommand.ExecuteCommand(nasa);
-Console.WriteLine(nasa.GetPositon());
-*/
+Console.WriteLine(roscosmos.GetPositon());
 
 
 
